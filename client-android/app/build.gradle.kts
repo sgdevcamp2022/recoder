4b02e433c92f4094448acab28ccd136b4fb50a05
uzun <hyjhyj0901@pusan.ac.kr>
2023-01-17 12:42:21 +0900

    REC-24 [app][init] 안드로이드 프로젝트 개발환경 설정

---
+++ client-android/app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+//    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.recoder.reco"
+    compileSdk = Configurations.compileSdk
+
+    defaultConfig {
+        applicationId = "com.recoder.reco"
+        minSdk = Configurations.minSdk
+        targetSdk = Configurations.targetSdk
+        versionCode = Configurations.versionCode
+        versionName = Configurations.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true // Enables code shrinking for the release build type.
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = Configurations.jvmTarget
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+    packagingOptions {
+        resources.excludes.add("META-INF/LICENSE*")
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":presentation"))
+    implementation(project(":data"))
+
+}
