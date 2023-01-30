36a18bcdb7b682e390b5c1161590514c74d35e6d
uzun <hyjhyj0901@pusan.ac.kr>
2023-01-30 20:34:51 +0900

    feat : 대기실(waiting) xml ui 구현

--- client-android/presentation/build.gradle.kts
+++ client-android/presentation/build.gradle.kts
@@ -39,6 +39,9 @@ android {
 dependencies {
     implementation(project(":domain"))
     implementation(Libraries.VIEW_LIBRARIES)
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     kapt(Kapt.VIEW_LIBRARIES)
     androidTestImplementation(AndroidTestImpl.VIEW_LIBRARIES)
 //    debugImplementation(DebugImpl.VIEW_LIBRARIES)
---
+++ client-android/presentation/src/main/java/com/recoder/presentation/ui/waiting/WaitingRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.recoder.presentation.ui.waiting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.recoder.presentation.R
+
+class WaitingRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waiting_room)
+    }
+}
