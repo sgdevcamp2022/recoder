baa4098991852a74364c12a79345923848e8122e
YooJun Hong <46425142+Kick-snare@users.noreply.github.com>
2023-01-31 18:08:33 +0900

    [APP] Feature/#15 join by code 코드로 참가 UI (#18)
    * feat: 코드로 참가 joinByCode 프래그먼트 추가
    
    * chore: theme 설정 및 theme 수정
    
    * feat : home to joinByCode 네비게이팅
    
    * style : resource 추가
    
    * feat: edit text field Theme 에 맞게 material화

--- client-android/presentation/src/main/java/com/recoder/presentation/ui/home/HomeFragment.kt
+++ client-android/presentation/src/main/java/com/recoder/presentation/ui/home/HomeFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.recoder.presentation.R
+import com.recoder.presentation.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,8 +17,11 @@ private const val ARG_PARAM2 = "param2"
 
 class HomeFragment : Fragment() {
 
+    private val binding by viewBinding(FragmentHomeBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -23,5 +29,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnJoinByCode.setOnClickListener {
+            it.findNavController().navigate(R.id.action_home_fragment_to_join_by_code_fragment)
+        }
     }
 }
