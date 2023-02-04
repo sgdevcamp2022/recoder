bbcf69c5125fb9cafc8c1aea81a34bd75349b4e9
uzun <hyjhyj0901@pusan.ac.kr>
2023-02-04 16:12:12 +0900

    feat : 로깅 트리 커스텀
    -> filename : linenumber

--- client-android/app/src/main/java/com/recoder/comeet/ComeetApplication.kt
+++ client-android/app/src/main/java/com/recoder/comeet/ComeetApplication.kt
@@ -8,7 +8,13 @@ class ComeetApplication : Application() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(CustomTimberTree())
         }
+    }
+}
+
+class CustomTimberTree : Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement): String {
+        return "${element.fileName}:${element.lineNumber}"
     }
 }
