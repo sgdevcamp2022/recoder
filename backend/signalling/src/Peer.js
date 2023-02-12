618c4ee2438b2376a46b9043edca6b47c6f9080d
s2코딩천사선영이s2 <57889360+cpplovelove@users.noreply.github.com>
2023-02-12 11:48:10 +0900

    [SERVER] Feature/#41 사용하지 않는 함수 제거 (#56)
    * refactor: ds store 제거
    
    * refactor: 사용하지 않는 함수 제거

--- backend/signalling/src/Peer.js
+++ backend/signalling/src/Peer.js
@@ -2,157 +2,148 @@ import Logger from './Logger.js';
 const log = new Logger('Peer');
 
 export default class Peer {
-    constructor(socket_id, data) {
-        this.id = socket_id;
-        this.peer_info = data.peer_info;
-        this.peer_name = data.peer_info.peer_name;
-        this.peer_audio = data.peer_info.peer_audio;
-        this.peer_video = data.peer_info.peer_video;
-        this.peer_video_privacy = data.peer_video_privacy;
-        this.peer_hand = data.peer_info.peer_hand;
-        this.transports = new Map();
-        this.consumers = new Map();
-        this.producers = new Map();
+  constructor(socket_id, data) {
+    this.id = socket_id;
+    this.peer_info = data.peer_info;
+    this.peer_name = data.peer_info.peer_name;
+    this.peer_audio = data.peer_info.peer_audio;
+    this.peer_video = data.peer_info.peer_video;
+    this.peer_hand = data.peer_info.peer_hand;
+    this.transports = new Map();
+    this.consumers = new Map();
+    this.producers = new Map();
+  }
+
+  // Update peer info
+  updatePeerInfo(data) {
+    log.debug('Update peer info', data);
+    switch (data.type) {
+      case 'audio':
+      case 'audioType':
+        this.peer_info.peer_audio = data.status;
+        this.peer_audio = data.status;
+        break;
+      case 'video':
+      case 'videoType':
+        this.peer_info.peer_video = data.status;
+        this.peer_video = data.status;
+        break;
+      case 'screen':
+      case 'screenType':
+        this.peer_info.peer_screen = data.status;
+        break;
+      case 'hand':
+        this.peer_info.peer_hand = data.status;
+        this.peer_hand = data.status;
+        break;
     }
+  }
 
-    // Update peer info
-    updatePeerInfo(data) {
-        log.debug('Update peer info', data);
-        switch (data.type) {
-            case 'audio':
-            case 'audioType':
-                this.peer_info.peer_audio = data.status;
-                this.peer_audio = data.status;
-                break;
-            case 'video':
-            case 'videoType':
-                this.peer_info.peer_video = data.status;
-                this.peer_video = data.status;
-                if (data.status == false) {
-                    this.peer_info.peer_video_privacy = data.status;
-                    this.peer_video_privacy = data.status;
-                }
-                break;
-            case 'screen':
-            case 'screenType':
-                this.peer_info.peer_screen = data.status;
-                break;
-            case 'hand':
-                this.peer_info.peer_hand = data.status;
-                this.peer_hand = data.status;
-                break;
-            case 'privacy':
-                this.peer_info.peer_video_privacy = data.status;
-                this.peer_video_privacy = data.status;
-                break;
-        }
-    }
+  // WebRTC Transport
+  addTransport(transport) {
+    this.transports.set(transport.id, transport);
+  }
 
-    // WebRTC Transport
-    addTransport(transport) {
-        this.transports.set(transport.id, transport);
-    }
+  async connectTransport(transport_id, dtlsParameters) {
+    if (!this.transports.has(transport_id)) return;
 
-    async connectTransport(transport_id, dtlsParameters) {
-        if (!this.transports.has(transport_id)) return;
+    await this.transports.get(transport_id).connect({
+      dtlsParameters: dtlsParameters
+    });
+  }
 
-        await this.transports.get(transport_id).connect({
-            dtlsParameters: dtlsParameters,
+  close() {
+    this.transports.forEach((transport) => transport.close());
+  }
+
+  // Producer
+  getProducer(producer_id) {
+    return this.producers.get(producer_id);
+  }
+
+  async createProducer(producerTransportId, rtpParameters, kind, type) {
+    let producer = await this.transports.get(producerTransportId).produce({
+      kind,
+      rtpParameters
+    });
+
+    producer.appData.mediaType = type;
+
+    this.producers.set(producer.id, producer);
+
+    producer.on(
+      'transportclose',
+      function () {
+        log.debug('Producer transport close', {
+          peer_name: this.peer_info.peer_name,
+          consumer_id: producer.id
         });
+        producer.close();
+        this.producers.delete(producer.id);
+      }.bind(this)
+    );
+
+    return producer;
+  }
+
+  closeProducer(producer_id) {
+    try {
+      this.producers.get(producer_id).close();
+    } catch (ex) {
+      log.warn('Close Producer', ex);
+    }
+    this.producers.delete(producer_id);
+  }
+
+  // Consumer
+  async createConsumer(consumer_transport_id, producer_id, rtpCapabilities) {
+    let consumerTransport = this.transports.get(consumer_transport_id);
+    let consumer = null;
+
+    try {
+      consumer = await consumerTransport.consume({
+        producerId: producer_id,
+        rtpCapabilities,
+        paused: false
+      });
+    } catch (error) {
+      return console.error('Consume failed', error);
     }
 
-    close() {
-        this.transports.forEach((transport) => transport.close());
+    if (consumer.type === 'simulcast') {
+      await consumer.setPreferredLayers({
+        spatialLayer: 2,
+        temporalLayer: 2
+      });
     }
 
-    // Producer
-    getProducer(producer_id) {
-        return this.producers.get(producer_id);
-    }
+    this.consumers.set(consumer.id, consumer);
 
-    async createProducer(producerTransportId, rtpParameters, kind, type) {
-        let producer = await this.transports.get(producerTransportId).produce({
-            kind,
-            rtpParameters,
+    consumer.on(
+      'transportclose',
+      function () {
+        log.debug('Consumer transport close', {
+          peer_name: this.peer_info.peer_name,
+          consumer_id: consumer.id
         });
+        this.consumers.delete(consumer.id);
+      }.bind(this)
+    );
 
-        producer.appData.mediaType = type;
+    return {
+      consumer,
+      params: {
+        producerId: producer_id,
+        id: consumer.id,
+        kind: consumer.kind,
+        rtpParameters: consumer.rtpParameters,
+        type: consumer.type,
+        producerPaused: consumer.producerPaused
+      }
+    };
+  }
 
-        this.producers.set(producer.id, producer);
-
-        producer.on(
-            'transportclose',
-            function () {
-                log.debug('Producer transport close', {
-                    peer_name: this.peer_info.peer_name,
-                    consumer_id: producer.id,
-                });
-                producer.close();
-                this.producers.delete(producer.id);
-            }.bind(this),
-        );
-
-        return producer;
-    }
-
-    closeProducer(producer_id) {
-        try {
-            this.producers.get(producer_id).close();
-        } catch (ex) {
-            log.warn('Close Producer', ex);
-        }
-        this.producers.delete(producer_id);
-    }
-
-    // Consumer
-    async createConsumer(consumer_transport_id, producer_id, rtpCapabilities) {
-        let consumerTransport = this.transports.get(consumer_transport_id);
-        let consumer = null;
-
-        try {
-            consumer = await consumerTransport.consume({
-                producerId: producer_id,
-                rtpCapabilities,
-                paused: false,
-            });
-        } catch (error) {
-            return console.error('Consume failed', error);
-        }
-
-        if (consumer.type === 'simulcast') {
-            await consumer.setPreferredLayers({
-                spatialLayer: 2,
-                temporalLayer: 2,
-            });
-        }
-
-        this.consumers.set(consumer.id, consumer);
-
-        consumer.on(
-            'transportclose',
-            function () {
-                log.debug('Consumer transport close', {
-                    peer_name: this.peer_info.peer_name,
-                    consumer_id: consumer.id,
-                });
-                this.consumers.delete(consumer.id);
-            }.bind(this),
-        );
-
-        return {
-            consumer,
-            params: {
-                producerId: producer_id,
-                id: consumer.id,
-                kind: consumer.kind,
-                rtpParameters: consumer.rtpParameters,
-                type: consumer.type,
-                producerPaused: consumer.producerPaused,
-            },
-        };
-    }
-
-    removeConsumer(consumer_id) {
-        this.consumers.delete(consumer_id);
-    }
-};
+  removeConsumer(consumer_id) {
+    this.consumers.delete(consumer_id);
+  }
+}
