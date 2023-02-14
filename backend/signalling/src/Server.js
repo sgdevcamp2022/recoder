76556595cae2b304375c78f19e6637fbc39828b2
cpplovelove <gsgslolo@naver.com>
2023-02-14 21:07:38 +0900

    bug: 두번째 참가자 socket 객체에 room_id 삽입

--- backend/signalling/src/Server.js
+++ backend/signalling/src/Server.js
@@ -188,6 +188,8 @@ function startServer() {
     });
 
     socket.on('join', (data, cb) => {
+      socket.room_id = data.peer_info.room_id;
+
       if (!roomList.has(socket.room_id)) {
         return cb({
           error: 'Room does not exist'
