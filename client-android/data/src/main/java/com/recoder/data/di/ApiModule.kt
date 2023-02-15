abe3f08a8a3f69ca7ee123cd072299e0c1ebe993
uzun <hyjhyj0901@pusan.ac.kr>
2023-02-15 17:12:12 +0900

    feat : Scalars Retrofit 추가 구현 및 의존성 주입 지정

--- client-android/data/src/main/java/com/recoder/data/di/ApiModule.kt
+++ client-android/data/src/main/java/com/recoder/data/di/ApiModule.kt
@@ -1,18 +1,13 @@
 package com.recoder.data.di
 
+import com.recoder.data.di.NetworkModule.ScalarRetrofit
 import com.recoder.data.network.MeetingApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import timber.log.Timber
-import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -20,7 +15,9 @@ object ApiModule {
 
     @Singleton
     @Provides
-    fun provideMeetingApi(retrofit: Retrofit): MeetingApi
+    fun provideMeetingApi(
+        @ScalarRetrofit retrofit: Retrofit
+    ): MeetingApi
         = retrofit.create(MeetingApi::class.java)
 
 }
