3472f7700bd8d24d32532c92b17b7b045afb820e
정민석 <zxcvasdf1126@naver.com>
2023-02-15 20:50:19 +0900

    [SERVER] Feature/#71 PlanText to JSON (#72)

--- backend/signalling/controller/index.js
+++ backend/signalling/controller/index.js
@@ -4,7 +4,7 @@ import { v4 as uuidV4 } from 'uuid';
 const router = express.Router();
 router.get('/createLink', (req, res) => {
   const link = 'https://' + req.headers.host + '/' + uuidV4().slice(5, 17);
-  res.send(link);
+  res.json({ link });
 });
 
 export default router;
